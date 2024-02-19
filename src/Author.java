22f0d414ee68c20bdc7bead3535a849569537da6
Dany.Gen <Gentiev@vk.com>
2024-02-19 21:10:52 +0300

    HW.13

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String authorName;
     private String authorFemale;
@@ -21,5 +23,23 @@ public class Author {
 
     public void setAuthorFemale(String authorFemale) {
         this.authorFemale = authorFemale;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор произведения - " + authorName + " " + authorFemale + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(authorName, author.authorName) && Objects.equals(authorFemale, author.authorFemale);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authorName, authorFemale);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Author lev = new Author("lev","tolstiy");
-        Book theNightIsBezze = new Book("The Night", lev, 1912);
+        Author fitzdzherald = new Author("Fitzdzherald","tolstiy");
+        Book theNightIsBezze = new Book("The Night", fitzdzherald, 1912);
         System.out.println(theNightIsBezze.getNameBook());
-        System.out.println("author: " + lev.getAuthorName() + " " + lev.getAuthorFemale());
+        System.out.println("author: " + fitzdzherald.getAuthorName() + " " + fitzdzherald.getAuthorFemale());
         System.out.println(theNightIsBezze.getYearBook());
 
         theNightIsBezze.setYearBook(2000);
@@ -11,9 +11,20 @@ public class Main {
         System.out.println("--------------------");
 
         Author anacondaz = new Author("Anacondaz",".ru");
-        Book cdmm = new Book("Call me back", lev, 2016);
+        Book cdmm = new Book("Call me back", anacondaz, 2016);
         System.out.println("album: " + cdmm.getNameBook());
         System.out.println("author: " + anacondaz.getAuthorName() + anacondaz.getAuthorFemale());
         System.out.println(cdmm.getYearBook());
+
+        System.out.println(anacondaz.toString());
+        System.out.println(cdmm.toString());
+        System.out.println(fitzdzherald.equals(anacondaz));
+        System.out.println(theNightIsBezze.equals(cdmm));
+
+        Author anaconda = new Author("Anacondaz",".ru");
+        Book cdmmm = new Book("Call me back", anaconda, 2026);
+        System.out.println(anaconda.equals(anacondaz));
+        System.out.println(cdmmm.equals(cdmm));
+
     }
 }
